d04a35cda6fe4c232a8d73aac7060780a7339274
FastiGun <benjamin760080@gmail.com>
2023-07-13 10:49:42 +0200

    Création du bouton pour se logout et animations. Nav 90% OK

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,74 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.scss';
+//import Accueil from './Pages/Accueil/Accueil';
+import { useState } from 'react';
+
 
 function App() {
+
+  const [showLogOut, setShowLogOut] = useState(false);
+  //const [loged, setLoged] = useState(false);
+
+  const handleClickLogOut = () => {
+    setShowLogOut(!showLogOut);
+  }
+
+  const handleClickLoged = () => {
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <div className="gauche">
-          <h1>
-            <a href='/'>StayBook</a>
-          </h1>
-          <nav className='nav-home'>
-            <a href="/">Accueil</a>
-            <a href="/calendrier">Calendrier</a>
-          </nav>
+    <Router>
+      <div>
+        <div className="App">
+          <header className="App-header">
+            <div className="gauche">
+              <h1>
+                <a href='/accueil'>StayBook</a>
+              </h1>
+              <nav className='nav-home'>
+                <a href="/accueil">Accueil</a>
+                <a href="/calendrier">Calendrier</a>
+              </nav>
+            </div>
+            <div className="droite" onClick={handleClickLogOut}>
+              <div className="initiale">
+                <p>B</p>
+              </div>
+
+            </div>
+            {showLogOut && (
+              <div className="logout">
+                <p href="#" onClick={handleClickLoged}>
+                  Déconnexion
+                </p>
+              </div>
+            )}
+          </header>
         </div>
-        <div className="droite">
-          <div className="initiale">
-            <p>B</p>
-          </div>
+        <div className="content">
+          <Routes>
+            <Route exact path="/accueil" element={<Accueil />} />
+            <Route exact path="/calendrier" element={<Calendrier />} />
+            <Route exact path="/reservations" element={<Accueil />} />
+            <Route path="/*" element={<PageIntrouvable />} />
+          </Routes>
         </div>
-      </header>
-    </div>
+      </div>
+    </Router>
   );
+}
+
+function Accueil() {
+  return <h2>Accueil</h2>;
+}
+
+function Calendrier() {
+  return <h2>Calendrier</h2>;
+}
+
+function PageIntrouvable() {
+  return <h2>Page introuvable</h2>;
 }
 
 export default App;
